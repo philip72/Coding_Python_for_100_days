8a8a07cbb39645bb71d075187d0b35c408aaaf29
philip72 <52932034+philip72@users.noreply.github.com>
2024-03-05 22:00:59 +0000

    paddle

--- day-22 pong game/main.py
+++ day-22 pong game/main.py
@@ -1,13 +1,21 @@
 from turtle import Turtle, Screen
-import paddle
+from paddle import Paddle
 
 
 screen=Screen()
 screen.bgcolor('black')
 screen.setup(width=800,height=600)
 screen.title('Pong Game')
+screen.tracer(0)
+
+paddle_right= Paddle()
+paddle_right.create_right_paddle()
 
 screen.listen()
-screen.onkey(.up, "Up")
-screen.onkey(snake.down, "Down")
+screen.onkey(paddle_right.up, 'Up')
+screen.onkey(paddle_right.down, 'Down')
+game_on=True
+while game_on:
+    screen.update()
+
 screen.exitonclick()
--- day-22 pong game/paddle.py
+++ day-22 pong game/paddle.py
@@ -1,23 +1,28 @@
 from turtle import Turtle 
 
+shapesize= (5,1,0)
 UP=90
 DOWN=270
-paddle_right=Turtle()
-paddle_right.color('white')
-paddle_right.shape('rectangle')
-paddle_right.shapesize(stretch_wid=20,stretch_len=100)
-paddle_right.goto(x=350,y=0)
-
-def up(self):
-        if set.head.heading() != DOWN:
-            self.head.setheading(UP)
-def down(self):
-        if set.head.heading() != UP:
-            self.head.setheading(DOWN)
-
 
 class Paddle:
-      def __init__(self) -> None:
-            self.create_paddle()
-      def create_paddle(self):
-            padd
+    def __init__(self) -> None:
+            self.create_right_paddle()
+            self.create_left_paddle()
+    def create_right_paddle(self):
+            paddle= Turtle()
+            paddle.color('white')
+            paddle.shape('square')
+            paddle.shapesize(shapesize)
+            paddle.penup()
+            paddle.goto(x=350,y=0)
+    def create_left_paddle():
+          pass
+    def move(self):
+         pass
+    def up(self):
+        new_y= paddle.ycor()+20
+        paddle.goto(paddle.xcor(),new_y)
+
+    def down(self):
+        new_y= paddle.ycor()-20
+        paddle.goto(paddle.xcor(),new_y)
