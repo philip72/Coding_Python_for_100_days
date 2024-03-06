332ab0f23ad878c93a9b84b9f0761111889c98b1
philip72 <philipwamai85@gmail.com>
2024-03-06 22:15:21 +0300

    start ping pong

--- day-22 pong game/main.py
+++ day-22 pong game/main.py
@@ -6,16 +6,16 @@ screen=Screen()
 screen.bgcolor('black')
 screen.setup(width=800,height=600)
 screen.title('Pong Game')
-screen.tracer(0)
+# screen.tracer(0)
 
-paddle_right= Paddle()
-paddle_right.create_right_paddle()
+# paddle_right= Paddle()
+# paddle_right.create_right_paddle()
 
-screen.listen()
-screen.onkey(paddle_right.up, 'Up')
-screen.onkey(paddle_right.down, 'Down')
-game_on=True
-while game_on:
-    screen.update()
+# screen.listen()
+# screen.onkey(paddle_right.up, 'Up')
+# screen.onkey(paddle_right.down, 'Down')
+# game_on=True
+# while game_on:
+#     screen.update()
 
 screen.exitonclick()
---
+++ day-22 pong game/tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+screen.tracer(0)
+
+# paddle_right= Paddle()
+# paddle_right.create_right_paddle()
+
+# screen.listen()
+# screen.onkey(paddle_right.up, 'Up')
+# screen.onkey(paddle_right.down, 'Down')
+# game_on=True
+# while game_on:
+#     screen.update()
