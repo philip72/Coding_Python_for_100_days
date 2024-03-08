12d2d243eb5c7f11cc6a31dd312fe333877b61a3
philip72 <52932034+philip72@users.noreply.github.com>
2024-03-08 20:17:57 +0000

    comments

--- day-22 pong game/main.py
+++ day-22 pong game/main.py
@@ -1,3 +1,4 @@
+# Import necessary modules
 from turtle import Turtle, Screen
 from paddle import Paddle
 from pong_ball import PongBall
@@ -5,51 +6,22 @@ from scoreboard import Scoreboard
 import time
 
 
-screen = Screen()
+screen=Screen()
 screen.bgcolor('black')
 screen.setup(width=800, height=600)
 screen.title('Pong Game')
 screen.tracer(0)
-score_board= Scoreboard()
-right_paddle = Paddle(350, 0)  # Initialize with starting coordinates
-left_paddle = Paddle(-350, 0)  # Initialize with starting coordinates
 
-pong_ball=PongBall()
+paddle_right= Paddle()
+paddle_right.create_right_paddle()
 
+# Listen for keyboard input
 screen.listen()
-# Move right paddle
-screen.onkey(right_paddle.go_up,'Up')
-screen.onkey(right_paddle.go_down,'Down')
-
-# Move left paddle
-screen.onkey(left_paddle.go_up, 'w')
-screen.onkey(left_paddle.go_down, 's')
-
-game_on = True
-
+screen.onkey(paddle_right.up, 'Up')
+screen.onkey(paddle_right.down, 'Down')
+game_on=True
 while game_on:
-    time.sleep(0.1)
     screen.update()
-    pong_ball.move_ball()
 
-    #detect collision with wall
-    #past the 300 or -300 then its too far up then its a collision
-
-    if pong_ball.ycor() > 300 or pong_ball.ycor()<-300:
-        #needs to bounce
-        pong_ball.bounce_y()
-    
-    #dfetect collison with right_paddle
-    if pong_ball.distance(right_paddle)<50 and pong_ball.xcor()>320 or pong_ball.distance(left_paddle)<50 and pong_ball.xcor()<-320:
-        pong_ball.bounce_x()
-
-    #when r missed
-    if pong_ball.xcor()>380:
-        pong_ball.reset_position()
-        score_board.l_point()
-    #when l missed
-    if pong_ball.xcor()<-380:
-        pong_ball.reset_position()
-        score_board.r_point()
-
+# Exit the game when the user clicks anywhere
 screen.exitonclick()
