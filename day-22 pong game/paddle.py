c0516f19d76af6bb787ce0f54f61e2a148f8508e
philip72 <philipwamai85@gmail.com>
2024-03-11 21:31:54 +0300

    finished day 22 pong game

--- day-22 pong game/paddle.py
+++ day-22 pong game/paddle.py
@@ -1,28 +1,25 @@
-from turtle import Turtle 
+from turtle import Turtle
 
-shapesize= (5,1,0)
-UP=90
-DOWN=270
+class Paddle(Turtle):
+    def __init__(self, x, y):
+        super().__init__()
+        self.create_paddle(x, y)#here it takes tuples
 
-class Paddle:
-    def __init__(self) -> None:
-            self.create_right_paddle()
-            self.create_left_paddle()
-    def create_right_paddle(self):
-            paddle= Turtle()
-            paddle.color('white')
-            paddle.shape('square')
-            paddle.shapesize(shapesize)
-            paddle.penup()
-            paddle.goto(x=350,y=0)
-    def create_left_paddle():
-          pass
-    def move(self):
-         pass
-    def up(self):
-        new_y= paddle.ycor()+20
-        paddle.goto(paddle.xcor(),new_y)
+    def create_paddle(self, x_coordinate, y_coordinate):
+        self.x = x_coordinate
+        self.y = y_coordinate
+        #we got rid of creating a new Turtle cause already the class carries the Turtle class
+        self.shape('square')
+        self.color('white')
+        self.penup()
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.goto(x_coordinate, y_coordinate)
+        
 
-    def down(self):
-        new_y= paddle.ycor()-20
-        paddle.goto(paddle.xcor(),new_y)
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
