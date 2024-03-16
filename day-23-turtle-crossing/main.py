7dd2706f5eb7270152112454287afc2f4db79bce
philip72 <philipwamai85@gmail.com>
2024-03-16 22:33:56 +0300

    positioned the turtle at the bottom

--- day-23-turtle-crossing/main.py
+++ day-23-turtle-crossing/main.py
@@ -1,7 +1,20 @@
 from turtle import Turtle, Screen
 
 screen= Screen()
+screen.bgcolor('black')
 turtle1=Turtle(shape='square')
+
+turtle1.color('yellow')
+
+turtle1.penup()
+screen.tracer(0)
+
+game_on=True
+
+while game_on:
+    screen.update()
+    turtle1.goto(0,-330)
+
 
 
 screen.exitonclick()
