dcd4b685fb336bac8e3656f2546e0103023db552
philip72 <52932034+philip72@users.noreply.github.com>
2024-03-21 22:19:05 +0300

    Update cars.py

--- day-23-turtle-crossing/cars.py
+++ day-23-turtle-crossing/cars.py
@@ -2,4 +2,9 @@ from turtle import Turtle
 
 class Cars(Turtle):
   def __init__(self):
-    super. 
+    super().__init__()
+    self.shape('rectangle')
+    self.color('white')
+  def colors(self):
+    self.list_of_colors=[]
+    
