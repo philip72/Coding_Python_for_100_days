4bc86d685fc0ca45953e3c730442540d5b47a765
philip72 <philipwamai85@gmail.com>
2024-05-18 02:11:29 +0300

    File access and directory

--- day-20-21-snake-game/main.py
+++ day-20-21-snake-game/main.py
@@ -53,15 +53,14 @@ while game_is_on:
     
     #detect collision with wall
         if snake.head.xcor()> 280 or snake.head.xcor()<-280 or snake.head.ycor()>280 or snake.head.ycor()<-280:
-            game_is_on=False
-    
+            score.reset()   
     #detect collison  with tail, if head collides with any segment  in the tail
             for segment in snake.segments[1:]:
                 # if segment==snake.head:
                 #     pass
                 if snake.head.distance(segment)<10:
-                    game_is_on=False
-                    score.game_over()
+                    
+                    score.reset()
 
     
 
